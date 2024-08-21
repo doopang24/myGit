85ae51e329ace58ace3b63fd64f32beba275aaa1
wodus4145@naver.com <wodus4145@naver.com>
2024-08-22 00:04:10 +0900

    add saveTask, loadTask
    using Gson, JSON

--- javafx-todolist/src/main/java/sjy/firstproject/javafx_todolist/TodoListController.java
+++ javafx-todolist/src/main/java/sjy/firstproject/javafx_todolist/TodoListController.java
@@ -1,8 +1,18 @@
 package sjy.firstproject.javafx_todolist;
 
 import javafx.fxml.FXML;
+
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
 
 public class TodoListController {
 
@@ -13,12 +23,24 @@ public class TodoListController {
 	private ListView<String> taskList;
 	
 	@FXML
+	private TextField editField;
+	
+	private final String DATA_FILE = "tasks.json";
+	private final Gson gson = new Gson();
+	
+	@FXML
+	private void initialize( ) {
+		loadTasks();
+	}
+	
+	@FXML
 	private void handleAddTask() {
 		// 텍스트 필드에 입력된 값을 가져와서 리스트 뷰에 추가
 		String task = inputField.getText();
 		if(task!=null && !task.isEmpty()) {
 			taskList.getItems().add(task);
 			inputField.clear();
+			saveTask();
 		}
 	}
 	
@@ -27,11 +49,9 @@ public class TodoListController {
 		String selectedTask = taskList.getSelectionModel().getSelectedItem();
 		if(selectedTask!=null) {
 			taskList.getItems().remove(selectedTask);
+			saveTask();
 		}
 	}
-	
-	@FXML
-	private TextField editField;
 	
 	@FXML
 	private void handleEditTask() {
@@ -41,6 +61,27 @@ public class TodoListController {
 			int selectedIndex = taskList.getSelectionModel().getSelectedIndex();
 			taskList.getItems().set(selectedIndex, newTask);
 			editField.clear();
+			saveTask();
+		}
+	}
+	
+	private void saveTask() {
+		try (FileWriter writer = new FileWriter(DATA_FILE)) {
+			gson.toJson(taskList.getItems(), writer);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void loadTasks() {
+		try (FileReader reader = new FileReader(DATA_FILE)) {
+			Type type = new TypeToken<List<String>>() {}.getType();
+			List<String> tasks = gson.fromJson(reader, type);
+			if(tasks != null) {
+				taskList.getItems().addAll(tasks);
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
